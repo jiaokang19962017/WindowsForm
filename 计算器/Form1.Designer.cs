93885b687b997df05dbab60cd667f3ab5a28c9a9
焦康 <Administrator@HP201-1>
2017-06-22 17:20:56 +0800

    计算器

---
+++ 计算器/Form1.Designer.cs
@@ -0,0 +1,169 @@
+﻿namespace 计算器
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtnum1 = new System.Windows.Forms.TextBox();
+            this.txtnum2 = new System.Windows.Forms.TextBox();
+            this.txtsum = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.btn1 = new System.Windows.Forms.Button();
+            this.btn2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "第一个数";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(284, 21);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 12);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "第二个数";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(167, 21);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(41, 12);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "运算符";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(101, 75);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(29, 12);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "结果";
+            // 
+            // txtnum1
+            // 
+            this.txtnum1.Location = new System.Drawing.Point(14, 37);
+            this.txtnum1.Name = "txtnum1";
+            this.txtnum1.Size = new System.Drawing.Size(51, 21);
+            this.txtnum1.TabIndex = 4;
+            // 
+            // txtnum2
+            // 
+            this.txtnum2.Location = new System.Drawing.Point(286, 37);
+            this.txtnum2.Name = "txtnum2";
+            this.txtnum2.Size = new System.Drawing.Size(51, 21);
+            this.txtnum2.TabIndex = 5;
+            // 
+            // txtsum
+            // 
+            this.txtsum.Location = new System.Drawing.Point(157, 72);
+            this.txtsum.Name = "txtsum";
+            this.txtsum.Size = new System.Drawing.Size(62, 21);
+            this.txtsum.TabIndex = 6;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "+",
+            "-",
+            "*",
+            "/"});
+            this.comboBox1.Location = new System.Drawing.Point(145, 38);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(98, 20);
+            this.comboBox1.TabIndex = 7;
+            // 
+            // btn1
+            // 
+            this.btn1.Location = new System.Drawing.Point(55, 96);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(75, 23);
+            this.btn1.TabIndex = 8;
+            this.btn1.Text = "计算";
+            this.btn1.UseVisualStyleBackColor = true;
+            this.btn1.Click += new System.EventHandler(this.btn1_Click);
+            // 
+            // btn2
+            // 
+            this.btn2.Location = new System.Drawing.Point(262, 96);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(75, 23);
+            this.btn2.TabIndex = 9;
+            this.btn2.Text = "退出";
+            this.btn2.UseVisualStyleBackColor = true;
+            this.btn2.Click += new System.EventHandler(this.btn2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(381, 131);
+            this.Controls.Add(this.btn2);
+            this.Controls.Add(this.btn1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.txtsum);
+            this.Controls.Add(this.txtnum2);
+            this.Controls.Add(this.txtnum1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "简易计算器";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtnum1;
+        private System.Windows.Forms.TextBox txtnum2;
+        private System.Windows.Forms.TextBox txtsum;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.Button btn2;
+    }
+}
+
---
+++ 计算器/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace 计算器
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int sum;
+
+        private void btn1_Click(object sender, EventArgs e)
+        {
+            if (txtnum1.Text.Equals(string.Empty)&& txtnum2.Text.Equals(string.Empty)&&comboBox1.Text.Equals(string.Empty))
+            {
+                MessageBox.Show("为空");
+            }
+            else
+            {
+                if (comboBox1.Text == "+")
+                {
+                    sum = Convert.ToInt32(txtnum1.Text) + Convert.ToInt32(txtnum2.Text);
+                    txtsum.Text = Convert.ToString(sum);
+                }
+                else if (comboBox1.Text == "-")
+                {
+
+                    sum = Convert.ToInt32(txtnum1.Text) - Convert.ToInt32(txtnum2.Text);
+                    txtsum.Text = Convert.ToString(sum);
+                }
+                else if (comboBox1.Text == "*")
+                {
+
+                    sum = Convert.ToInt32(txtnum1.Text) * Convert.ToInt32(txtnum2.Text);
+                    txtsum.Text = Convert.ToString(sum);
+                }
+                else if (comboBox1.Text == "/")
+                {
+
+                    sum = Convert.ToInt32(txtnum1.Text) / Convert.ToInt32(txtnum2.Text);
+                    txtsum.Text = Convert.ToString(sum);
+                }
+            }
+
+            
+        }
+
+        private void btn2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
